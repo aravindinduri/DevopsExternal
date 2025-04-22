c50acbd31abe2ec8d51843af76b46b6a13d47b66
ARAVIND INDURI <thearavindinduri@gmail.com>
2025-04-22 09:28:16 +0530

    added changes

---
+++ testselenium.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testselenium {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
